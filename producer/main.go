74c27d11618f09dd30cd9920475d8df4fdbd6246
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-03-17 21:49:05 +0000

    commit

--- producer/main.go
+++ producer/main.go
@@ -1,2 +1,70 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IBM/sarama"
+)
+type order struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+func main() {
+	msgCnt := 0
+
+	worker, err := ConnectConsumer([]string{"localhost:9092"})
+	if err != nil {
+		panic(err)
+	}
+	defer worker.Close()
+
+	partitionConsumer, err := worker.ConsumePartition("orders", 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+	defer partitionConsumer.Close()
+
+	fmt.Print("worker started")
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-partitionConsumer.Errors():
+				fmt.Println(err)
+			case msg := <-partitionConsumer.Messages():
+				msgCnt++
+				fmt.Printf("Received order Count %d: | Topic(%s) | Message(%s) \n", msgCnt, string(msg.Topic), string(msg.Value))
+				order := string(msg.Value)
+				fmt.Printf("preparing order: %s\n", order)
+			case <-sigchan:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCnt, "messages")
+	
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+
+}
+
+func ConnectConsumer(broker []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer(broker, config)
+	return consumer, err
+}
